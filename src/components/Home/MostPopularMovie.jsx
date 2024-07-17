0da7675b8dbcb611d40c363b7ff8fa19b49492ee
davibcavalcante <davib506@gmail.com>
2024-07-17 00:01:03 -0300

    Corrigindo rota personalizada para deploy

--- src/components/Home/MostPopularMovie.jsx
+++ src/components/Home/MostPopularMovie.jsx
@@ -27,7 +27,7 @@ const MostPopularMovie = () => {
             <section className="w-full absolute bottom-40 left-1/2 -translate-x-1/2 z-40 sm:bottom-20">
                 <h1 className="text-white text-3xl text-center font-play font-semibold sm:text-5xl lg:text-7xl">{movie.title}</h1>
                 <h2 className="text-zinc-400 text-xl text-center font-raleway sm:text-2xl lg:text-3xl">{movie.release_date}</h2>
-                <Link to={`/movie/${movie.id}`} className="bg-red-600 text-white text-xl font-raleway font-bold block w-fit mx-auto my-4 px-6 py-1 rounded-sm shadow-xl lg:text-2xl">
+                <Link to={`/indigital-movies/movie/${movie.id}`} className="bg-red-600 text-white text-xl font-raleway font-bold block w-fit mx-auto my-4 px-6 py-1 rounded-sm shadow-xl lg:text-2xl">
                     Ver Mais
                 </Link>
             </section>
