662c2041ff23f6ba914a7852ef98c156b2accc91
davibcavalcante <davib506@gmail.com>
2024-09-30 19:58:59 -0300

    Bug fix: Exibindo botão 'home page' apenas filmes com sites

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -9,17 +9,17 @@ const Movie = () => {
     const { movie, error, loading } = useById(id);
 
     if (loading) {
-        return <div role="status" className="flex justify-center"><Loading/></div>
+        return <div role="status" className="flex justify-center"><Loading /></div>
     }
 
     if (error) {
         return <section>Erro na requisição</section>
     }
-    
+
     return (
         <section className={`relative h-screen overflow-y-auto py-12 px-4 md:flex md:items-center md:justify-center`}>
             <section className="fixed top-0 left-0 w-full h-full">
-                <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt="" className="min-h-full object-cover"/>
+                <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt="" className="min-h-full object-cover" />
             </section>
             <section className="w-full max-w-96 m-auto h-fit flex flex-col rounded-2xl shadow-xl overflow-hidden z-50 md:flex-row md:w-11/12 md:max-w-none lg:w-9/12">
                 <section className="w-full z-50 md:w-6/12 xl:w-4/12">
@@ -45,12 +45,13 @@ const Movie = () => {
                     </section>
                     <section className="mb-8 flex items-center justify-between">
                         <Link to="/indigital-movies" className="bg-red-600 text-white flex items-center justify-center aspect-square p-2 rounded-full"><Home /></Link>
-                        <Link to={movie.homepage} target="_blank" rel="external" className="bg-red-600 text-white font-semibold px-4 py-2 rounded-md">HOMEPAGE</Link>
+                        {movie.homepage &&
+                            <Link to={movie.homepage} target="_blank" rel="external" className="bg-red-600 text-white font-semibold px-4 py-2 rounded-md">HOMEPAGE</Link>}
                     </section>
                     <section className="bg-white p-4 rounded-xl shadow-xl flex items-center justify-center gap-4 xl:absolute xl:bottom-4 xl:left-1/2 xl:-translate-x-1/2 xl:w-10/12">
                         {movie.production_companies.map(item =>
                             item.logo_path !== null &&
-                                <img src={`https://image.tmdb.org/t/p/original/${item.logo_path}`} alt={item.name} key={item.name} className="w-2/6 max-w-16 max-h-16 overflow-x-auto xl:w-1/6" />
+                            <img src={`https://image.tmdb.org/t/p/original/${item.logo_path}`} alt={item.name} key={item.name} className="w-2/6 max-w-16 max-h-16 overflow-x-auto xl:w-1/6" />
                         )}
                     </section>
                 </section>
